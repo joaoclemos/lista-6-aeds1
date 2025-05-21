67846c07c4e0ac6b0c23d36f95e4db81773eac43
João Carvalho Lemos <jclemos@sga.pucminas.br>
2025-05-21 19:04:02 -0300

    alteração com falha ainda na Q5

--- q5.c
+++ q5.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+
 void preencherMatriz(int M[5][5])
 {
-    printf("Digite os elementos da matriz 5x5:\n");
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
         {
-            scanf("%d", &M[i][j]);
+            if (scanf("%d", &M[i][j]) != 1)
+            {
+                M[i][j] = 0;
+            }
         }
     }
 }
+
+void imprimirMatriz(int M[5][5])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int somaLinha4(int M[5][5])
 {
     int soma = 0;
     for (int j = 0; j < 5; j++)
     {
-        soma += M[3][j];
+        soma += M[4][j];
     }
     return soma;
 }
+
 int somaColuna2(int M[5][5])
 {
     int soma = 0;
@@ -29,24 +47,28 @@ int somaColuna2(int M[5][5])
     }
     return soma;
 }
-int somaDiagonalPrincipal(int M[5][5]) {
+
+int somaDiagonalPrincipal(int M[5][5])
+{
     int soma = 0;
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         soma += M[i][i];
     }
     return soma;
 }
 
-int somaDiagonalSecundaria(int M[5][5]) {
-    int soma = 0;
+long long somaDiagonalSecundaria(int M[5][5]) {
+    long long soma = 0;
     for (int i = 0; i < 5; i++) {
         soma += M[i][4 - i];
     }
     return soma;
 }
-int somaTodosElementos(int M[5][5])
+
+long long somaTodosElementos(int M[5][5])
 {
-    int soma = 0;
+    long long soma = 0;
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
@@ -56,16 +78,15 @@ int somaTodosElementos(int M[5][5])
     }
     return soma;
 }
+
 int main()
 {
     int M[5][5];
     preencherMatriz(M);
-    printf("\n%d\n", somaLinha4(M));
+    printf("%d\n", somaLinha4(M));
     printf("%d\n", somaColuna2(M));
     printf("%d\n", somaDiagonalPrincipal(M));
-    printf("%d\n", somaDiagonalSecundaria(M));
-    printf("%d\n", somaTodosElementos(M));
-
-
+    printf("%lld\n", somaDiagonalSecundaria(M));
+    printf("%lld\n", somaTodosElementos(M));
     return 0;
 }
