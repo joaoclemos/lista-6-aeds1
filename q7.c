bcd20131079d389b6c25b0ce7a1c7db7620d6e3c
João Carvalho Lemos <jclemos@sga.pucminas.br>
2025-05-21 20:08:13 -0300

    Q7

---
+++ q7.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+void preencherMatriz(int linhas, int colunas, int matriz[linhas][colunas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+}
+void imprimirMatriz(int linhas, int colunas, int M[linhas][colunas])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int A[4][6];
+    int B[4][6];
+    int S[4][6];
+    int D[4][6];
+    preencherMatriz(A);
+    preencherMatriz(B);
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<6; j++)
+        {
+            S[i][j] = A[i][j]+B[i][j];
+            D[i][j] = A[i][j]-B[i][j];
+            printf("%d", S[i][j]);
+        }
+    }
+    for(int=0; i<4; i++)
+    {
+        for(j=0; j<6; j++)
+        {
+            printf("%d", D[i][j]);
+        }
+    }
+    return 0;
+}
