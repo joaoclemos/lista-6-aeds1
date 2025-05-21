8294c159fa02b49379a93d1ba50ff9a8c951b180
João Carvalho Lemos <jclemos@sga.pucminas.br>
2025-05-21 05:32:35 -0300

    Alterações Q2

--- q2.c
+++ q2.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    int temp[32];
+    int temp[31];
     int maiortemp = 15;
     int menortemp = 45;
     float tempmedia = 0;
     int diasabaixo = 0;
 
-    for (int i=0; i<32; i++)
+    for (int i=0; i<31; i++)
     {
-        scanf("%d", temp[i]);
+        scanf("%d", &temp[i]);
         if (temp[i]>= maiortemp)
         {
             maiortemp=temp[i];
@@ -23,13 +23,13 @@ int main()
         tempmedia += temp[i];
     }
     tempmedia= tempmedia/31;
-    for(int j=0; j<32; j++){
-        if(nota[j]< tempmedia){
+    for(int j=0; j<31; j++){
+        if(temp[j]< tempmedia){
             diasabaixo++;
         }
     }
-printf("Menor e maior temperatura: %d e %d\n", menortemp maiortemp);
-printf("Media da temperatura: %f\n", tempmedia);
+printf("Menor e maior temperatura: %d e %d\n", menortemp, maiortemp);
+printf("Media da temperatura: %.2f\n", tempmedia);
 printf("Numero de dias nos quais a temperatura foi inferior a temperatura media: %d", diasabaixo);
 
 
