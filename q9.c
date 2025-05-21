558d6fb1c55d63384d6171e4ba141426433fdbf9
João Carvalho Lemos <jclemos@sga.pucminas.br>
2025-05-21 20:20:02 -0300

    Q9 e Q10 alterada

--- q9.c
+++ q9.c
@@ -3,7 +3,7 @@
 
 void ordenarVetor(int *vetor, int tamanho) {
     int i, j, temp;
-    
+
     for (i = 0; i < tamanho - 1; i++) {
         for (j = i + 1; j < tamanho; j++) {
             if (*(vetor + i) > *(vetor + j)) {
@@ -15,14 +15,13 @@ void ordenarVetor(int *vetor, int tamanho) {
     }
 }
 void imprimirVetor(int *vetor, int tamanho) {
-    printf("Vetor ordenado: [");
     for (int i = 0; i < tamanho; i++) {
         printf("%d", *(vetor + i));
         if (i < tamanho - 1) {
-            printf(", ");
+            printf(" ");
         }
     }
-    printf("]\n");
+    printf("\n");
 }
 int main() {
     int tamanho;
