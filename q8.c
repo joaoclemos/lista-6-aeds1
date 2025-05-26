2c92b933c72b2d362abd42cbbb692a756e69e224
João Carvalho Lemos <joaoclemos22@gmail.com>
2025-05-26 13:37:35 -0300

    Q8 com espaço

--- q8.c
+++ q8.c
@@ -11,7 +11,7 @@ void exibeMatriz(int M[10][10]) {
     int i, j;
     for(i = 0; i < 10; i++) {
         for(j = 0; j < 10; j++)
-            printf("%d", M[i][j]);
+            printf("%d ", M[i][j]);
     }
 }
 
