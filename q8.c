0497a9d3996d199fb444a04453db9119b801cfb0
João Carvalho Lemos <joaoclemos22@gmail.com>
2025-05-26 13:36:56 -0300

    Q8 resolvida

--- q8.c
+++ q8.c
@@ -11,10 +11,8 @@ void exibeMatriz(int M[10][10]) {
     int i, j;
     for(i = 0; i < 10; i++) {
         for(j = 0; j < 10; j++)
-            printf("%d ", M[i][j]);
-        printf("\n");
+            printf("%d", M[i][j]);
     }
-    printf("\n");
 }
 
 void trocaLinha2com8(int M[10][10]) {
