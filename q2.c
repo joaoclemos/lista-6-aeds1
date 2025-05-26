614a5b28cfb8bab9cd03bd4cbe157903c4e610bf
João Carvalho Lemos <joaoclemos22@gmail.com>
2025-05-26 13:35:22 -0300

    Q8

--- q2.c
+++ q2.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
     int temp[31];
-    int maiortemp, menortemp;
+    int maiortemp = 15;
+    int menortemp = 45;
     float tempmedia = 0;
     int diasabaixo = 0;
 
-    scanf("%d", &temp[0]);
-    maiortemp = menortemp = temp[0];
-    tempmedia = temp[0];
-
-    for (int i = 1; i < 31; i++) {
+    for (int i=0; i<31; i++)
+    {
         scanf("%d", &temp[i]);
-        if (temp[i] > maiortemp) maiortemp = temp[i];
-        if (temp[i] < menortemp) menortemp = temp[i];
+        if (temp[i]>= maiortemp)
+        {
+            maiortemp=temp[i];
+        }
+        if (temp[i]<= menortemp)
+        {
+            menortemp=temp[i];
+        }
         tempmedia += temp[i];
     }
-
-    tempmedia /= 31;
-
-    for (int j = 0; j < 31; j++) {
-        if (temp[j] < tempmedia) diasabaixo++;
+    tempmedia= tempmedia/31;
+    for(int j=0; j<31; j++){
+        if(temp[j]< tempmedia){
+            diasabaixo++;
+        }
     }
+printf("Menor e maior temperatura: %d e %d\n", menortemp, maiortemp);
+printf("Media de temperatura: %.2f\n", tempmedia);
+printf("Numero de dias nos quais a temperatura foi inferior a temperatura media: %d", diasabaixo);
 
-    printf("Menor e maior temperatura: %d e %d\n", menortemp, maiortemp);
-    printf("Media de temperatura: %.2f\n", tempmedia);
-    printf("Numero de dias nos quais a temperatura foi inferior a temperatura media: %d", diasabaixo);
+
 
     return 0;
 }
---
+++ q8.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void preencheMatriz(int M[10][10]) {
+    int i, j;
+    for(i = 0; i < 10; i++)
+        for(j = 0; j < 10; j++)
+            scanf("%d", &M[i][j]);
+}
+
+void exibeMatriz(int M[10][10]) {
+    int i, j;
+    for(i = 0; i < 10; i++) {
+        for(j = 0; j < 10; j++)
+            printf("%d ", M[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void trocaLinha2com8(int M[10][10]) {
+    int i, temp;
+    for(i = 0; i < 10; i++) {
+        temp = M[1][i];
+        M[1][i] = M[7][i];
+        M[7][i] = temp;
+    }
+}
+
+void trocaColuna4com10(int M[10][10]) {
+    int i, temp;
+    for(i = 0; i < 10; i++) {
+        temp = M[i][3];
+        M[i][3] = M[i][9];
+        M[i][9] = temp;
+    }
+}
+
+void trocaDiagonais(int M[10][10]) {
+    int i, temp;
+    for(i = 0; i < 10; i++) {
+        temp = M[i][i];
+        M[i][i] = M[i][9-i];
+        M[i][9-i] = temp;
+    }
+}
+
+void trocaLinha5comColuna10(int M[10][10]) {
+    int i, temp;
+    for(i = 0; i < 10; i++) {
+        temp = M[4][i];
+        M[4][i] = M[i][9];
+        M[i][9] = temp;
+    }
+}
+
+int main() {
+    int M[10][10];
+    preencheMatriz(M);
+    exibeMatriz(M);
+    trocaLinha2com8(M);
+    exibeMatriz(M);
+    preencheMatriz(M);
+    trocaColuna4com10(M);
+    exibeMatriz(M);
+    preencheMatriz(M);
+    trocaDiagonais(M);
+    exibeMatriz(M);
+    preencheMatriz(M);
+    trocaLinha5comColuna10(M);
+    exibeMatriz(M);
+    return 0;
+}
